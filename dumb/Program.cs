9aeee346a9e4ba7fba52b5ede3bfb97139cae2e8
Damien Chaib <dchaib@users.noreply.github.com>
2024-07-14 16:53:21 +0200

    feat: Create dumb tool

---
+++ dumb/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+var versionString = Assembly.GetEntryAssembly()?
+    .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+    .InformationalVersion
+    .ToString();
+
+Console.WriteLine($"Hello, this is version {versionString}!");
